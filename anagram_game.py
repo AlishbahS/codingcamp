3bcc3b55c861ad03845e4b493e5f3936943e7b7b
AlishbahS <as10294@st.habib.edu.pk>
2024-07-29 13:36:03 -0700

    Update anagram_game.py

--- anagram_game.py
+++ anagram_game.py
@@ -21,9 +21,25 @@ dict = {
 #Get a category(words/phrases) as input from user
 category= input("Enter a category(words/phrases) :").strip().lower()
 if category in dict:
-            print(f"Category selected: {category}") 
-        else:
-            print("Invalid category. Please enter 'words' or 'phrases'.")
+   print(f"Category selected: {category}") 
+else:
+   print("Invalid category. Please enter 'words' or 'phrases'.")
+
+def choose_word_or_phrase(category)
+"""The function will choose a random element from the selected category"""
+elements=list(dict[category].keys())
+# The key() shows all the elements in a specific category
+# To access an element of dict we use square brackets
+random_element=random.choice(elements)
+return random_element
+
+def shuffle(random_element)
+letters=list(random_element)
+random.shuffle(letters)
+shuffled=''.join(letters)
+#join() converts list back to string
+return shuffled
+
 
 
 
