edca527104b0b48dcf0029719ec227ea9cc086ce
AlishbahS <as10294@st.habib.edu.pk>
2024-07-31 08:47:25 -0700

    Update anagram_game.py

--- anagram_game.py
+++ anagram_game.py
@@ -18,41 +18,75 @@ dict = {
     }
 }
 
+def main():
 #Get a category(words/phrases) as input from user
-category= input("Enter a category(words/phrases) :").strip().lower()
-if category in dict:
-   print(f"Category selected: {category}") 
-else:
-   print("Invalid category. Please enter 'words' or 'phrases'.")
-
-def choose_word_or_phrase(category):
-    """The function will choose a random element from the selected category"""
-    elements=list(dict[category].keys())
-    # The key() shows all the elements in a specific category
-    # To access an element of dict we use square brackets
-    random_element=random.choice(elements)
-    return random_element
-
-random_element=choose_word_or_phrase(category)
-shuffled = shuffle(random_element)
-
-attempts=3
-while attempts!=0:
-      print("the word is",shuffled)
-
-guess="enter your guess:  ".lower().strip()
-    
-    if guess == random_element:
-        print("Congratulations! You guessed it right.")
-        break
-
+    category= input("Enter a category(words/phrases) :").strip().lower()
+    if category in dict:
+        print(f"Category selected: {category}") 
     else:
-        attempts -= 1
-        if attempts > 0:
-            print(f"Wrong guess. You have {attempts} attempts left.")
-        else:
-            print(f"Sorry, you're out of attempts. The correct answer was '{random_element}'.")
+        print("Invalid category. Please enter 'words' or 'phrases'.")
 
+    def choose_word_or_phrase(category):
+        """The function will choose a random element from the selected category"""
+        elements=list(dict[category].keys())
+        # The key() shows all the elements in a specific category
+        # To access an element of dict we use square brackets
+        random_element=random.choice(elements)
+        return random_element
+
+    def shuffle(random_element):
+        letters=list(random_element)
+        random.shuffle(letters)
+        shuffled=''.join(letters)
+        #join() converts list back to string
+        return shuffled
+
+
+    #storing return value into variable
+    random_element=choose_word_or_phrase(category)
+    shuffled=shuffle(random_element)
+
+    print(f"Solve {shuffled}")
+    hint=dict[category][random_element]
+    print(f"Hint: {hint}")
+
+
+    #to check if guess is correct or incorrect
+    for attempt in range(1,4):
+        guess=input("Enter your guess: ").strip().lower()   
+        if guess==random_element:
+            print("Welldone!You guessed correct.")
+            restart_()
+            break
+        else:
+            print ("You guessed wrong!")
+            attempt_left= 3-attempt
+
+    #to check the number of attempts left
+        if attempt_left>0:
+            print (f"You have {attempt_left} attempts left")
+        else:
+                print (f"Out of attempts!The correct answer was {random_element}")
+
+#restart the solver
+def restart_():
+    restart= input("Do you want to play (yes/no): ").strip().lower()
+    if restart=="no":
+        print("Thanks for playing")
+    elif restart=="yes":
+        main()
+    else:
+        print("Invalid answer")
+        print(restart)
+restart_()        
+
+
+
+    
+
+
+
+  
 
 
 
